fd36c8a7a1a95c7dabcfc730ca49f36bc689f140
goblinbot <th.boerma@gmail.com>
2017-11-07 11:51:46 +0100

    Opbouw admin grid & overnemen keypad van beacon

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ var fs = require('fs');
 /* INITIALISEN VAN APP */
 var port = process.env.PORT || 5002;
 
+var missions = [];
+
 app.use(express.static('public'));
 app.use(express.static('_includes'));
 app.use('_includes', express.static('public'));
@@ -34,3 +36,60 @@ app.get('/admin/', function(req, res){
 app.get('*', function(req, res){
   res.sendFile('404.html', {"root": __dirname+'/public/'});
 });
+
+io.on('connection', function (socket) {
+
+  setTimeout(function(){
+    /* send FRONTEND data to FRONT */
+    socket.emit('init_frontendBoard', missions);
+
+  },1000);
+
+
+
+
+
+
+  socket.on('auth', function(keycode){
+    var checklogincode = 0;
+    var loginrank = 0;
+    console.log('authentication code received: '+keycode);
+
+    for (var i in valid_accounts) {
+
+      if(valid_accounts[i].logincode == keycode) {
+        checklogincode = 1;
+        /*loginrank = valid_accounts[i].loginrank;*/
+      }
+    }
+
+    if(checklogincode == 1) {
+      socket.emit('authTrue', keycode, loginrank);
+    } else {
+      socket.emit('authFalse');
+    }
+  });
+
+});
+
+
+
+/* code for accounts */
+function accountObj(logincode,loginrank) {
+  this.logincode = logincode;
+  this.loginrank = loginrank;
+}
+var valid_accounts = [];
+
+/*
+add entries here to make accounts, bound to a five-digit code.
+For example:
+  valid_accounts[5] = (new accountObj('99999','1'));
+the second digit, which is '1' in our example, determines the user 'rank'. The higher, the better, going from 1 to 4.
+a full admin/gamemaster would be:
+  valid_accounts[0] = (new accountObj('00451','4'));
+*/
+valid_accounts[0] = (new accountObj('61021','4'));
+valid_accounts[2] = (new accountObj('45100','3'));
+valid_accounts[3] = (new accountObj('67790','1'));
+valid_accounts[4] = (new accountObj('34471','2'));
--- public/missionObj.js
+++ public/missionObj.js
@@ -1,6 +1,6 @@
 /*< =========================================== >
-  |
-  >
+  | Author: Thijs Boerma
+  > Mission board : mission object structure.
   |
   <
   |
@@ -9,23 +9,41 @@
   < =========================================== >*/
 
 function mission(id, priority, type, goal, title, authorisedBool, authorisedBy, debriefAgent, agentsJoined, agentsRequested, date, xo, reminderBool, reminderReason, additional ) {
+
+  var dt = new Date();
+  var currentHours   = dt.getHours ( );
+  var currentMinutes = dt.getMinutes ( );
+  var currentSeconds = dt.getSeconds ( );
+    currentHours = ( currentHours < 10 ? "0" : "" ) + currentHours;
+    currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+    currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+  currentTime = currentHours + ":" + currentMinutes + ":" + currentSeconds;
+
   this.title = id;
-  this.priority = priority; /* 0-5? */
-  this.type = type; /* type = bijv. combat, research, diplo. Kleuren/class baseren op deze types. */
-  this.goal = goal; /* max 20 woorden. */
-  this.title = title;
-  this.authorisedBool = authorisedBool; /* Y/N */
-  this.authorisedBy = authorisedBy;
-  this.debriefAgent = debriefAgent; /* de arme persoon die het moet debriefen. */
-  this.agentsJoined = agentsJoined;
-  this.agentsRequested = agentsRequested;
-  this.date = date;
-  this.xo = xo;
-  this.reminderBool = reminderBool; /* Reminder? */
-  this.reminderReason = reminderReason; /* en waarom dan? */
-  this.additional = additional; /* opmerkingen veld */
+  this.priority = 3; /* 0-5? */
+  this.type = 'combat'; /* type = bijv. combat, research, diplo. Kleuren/class baseren op deze types. */
+  this.goal = ''; /* max 20 woorden. */
+  this.title = 'title';
+  this.authorisedBool = 'N'; /* Y/N */
+  this.authorisedBy = 'none';
+  this.debriefAgent = ''; /* de arme persoon die het moet debriefen. */
+  this.agentsJoined = 0;
+  this.agentsRequested = 0;
+  this.date = '13-03-240NT';
+  this.time = currentTime;
+  this.xo = 'XO';
+  this.reminderBool = 'false'; /* Reminder? */
+  this.reminderReason = ''; /* en waarom dan? */
+  this.additional = ''; /* opmerkingen veld */
 }
 
-/* Systeem */
-var testdefault = new mission();
-testdefault['xo'] = 'meme';
+/* Test missie: heen en weer converten van. Hoezee. */
+var testMISS = new mission();
+  testMISS['xo'] = 'Maati';
+  testMISS['title'] = "TestMission";
+  testMISS['goal'] = "Testing the mission objects";
+  testMISS['priority'] = 4;
+
+var testMISSJSON = JSON.stringify(testMISS);
+var testMISSPARSE = JSON.parse(testMISSJSON);
